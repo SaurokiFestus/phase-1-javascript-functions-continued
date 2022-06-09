eb1eddd42ec390a1944902bf55f2571be54b4f54
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-06-10 01:41:03 +0300

    commit tree 5a2dd05aa2cd13d11fbb104869c6b20daa1ebd47

--- index.js
+++ index.js
@@ -1,14 +1,64 @@
 // code your solution here
+// function saturdayFun(activity = "roller-skate"){
+//     return `This Saturday, I want to ${activity}!`
+// }
+
+// function mondayWork(activity1= "go to the office"){
+//     return `This Monday, I will ${activity1}.`
+// }
+
+// let wrapAdjective = function(style="*") {
+//   return function(adjective="special") {
+//     return `You are ${style}${adjective}${style}!`
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function saturdayFun(activity = "roller-skate"){
-    return `This Saturday, I want to ${activity}!`
+  return `This Saturday, I want to ${activity}!`
+}
+// console.log(saturdayFun())
+
+function mondayWork(activity2 = "go to the office"){
+  return `This Monday, I will ${activity2}.`
 }
 
-function mondayWork(activity1= "go to the office"){
-    return `This Monday, I will ${activity1}.`
-}
-
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
+let wrapAdjective= function (string = "*"){
+  return function (adjective = "special"){
+    return `You are ${string}${adjective}${string}!`
   }
+  
 }
